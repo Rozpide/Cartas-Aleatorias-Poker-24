211204d228e9f10af0fbe27346ef0cd6618d2d27
Rozpi <100348581+Rozpide@users.noreply.github.com>
2024-12-06 22:15:10 +0000

    carta poker uso bootstrap

--- src/app.js
+++ src/app.js
@@ -1,32 +1,71 @@
 /* eslint-disable */
 import "bootstrap";
 import "./style.css";
-
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  let numeroAleatorioParaCarta = [
-    "1",
-    "2",
-    "3",
-    "4",
-    "5",
-    "6",
-    "7",
-    "8",
-    "9",
-    "J",
-    "Q",
-    "K"
-  ];
-  document.querySelector(
-    ".remplazar-numero"
-  ).innerHTML = numeroAleatorioParaCarta;
-  let numeroAleatorioParaPalo = Math.floor(Math.random() * 4) + 1;
-  console.log(numeroAleatorioParaPalo);
-  numeroAleatorioParaCarta = Math.floor(Math.random() * 12) + 1;
-  console.log(numeroAleatorioParaCarta);
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  function generarCarta() {
+    let numeroAleatorioParaCarta = [
+      "A",
+      "2",
+      "3",
+      "4",
+      "5",
+      "6",
+      "7",
+      "8",
+      "9",
+      "10",
+      "J",
+      "Q",
+      "K"
+    ];
+    let numeroAleatorioParaPalo = [
+      { palo: "♠", clase: "spade" },
+      { palo: "♣", clase: "club" },
+      { palo: "♥", clase: "heart" },
+      { palo: "♦", clase: "diamond" }
+    ];
+
+    function randomCartas(min, max) {
+      return Math.floor(Math.random() * (max - min) + min);
+    }
+
+    let carta =
+      numeroAleatorioParaCarta[
+        randomCartas(0, numeroAleatorioParaCarta.length)
+      ];
+    let paloInfo =
+      numeroAleatorioParaPalo[randomCartas(0, numeroAleatorioParaPalo.length)];
+
+    let numeroElemento = document.querySelector(".remplazar-numero");
+    let topPaloElemento = document.querySelector(".top-left");
+    let bottomPaloElemento = document.querySelector(".bottom-right");
+    let cardElemento = document.querySelector(".card");
+
+    if (
+      numeroElemento &&
+      topPaloElemento &&
+      bottomPaloElemento &&
+      cardElemento
+    ) {
+      numeroElemento.innerHTML = carta;
+      topPaloElemento.innerHTML = paloInfo.palo;
+      bottomPaloElemento.innerHTML = paloInfo.palo;
+
+      // Remover clases de palo anteriores
+      cardElemento.classList.remove("spade", "club", "heart", "diamond");
+
+      // Agregar la nueva clase de palo
+      cardElemento.classList.add(paloInfo.clase);
+    } else {
+      console.error("Uno o más elementos no fueron encontrados en el DOM.");
+    }
+  }
+
+  document.querySelector("#boton").addEventListener("click", generarCarta);
+
+  // Generar una carta al cargar la página
+  generarCarta();
 };
