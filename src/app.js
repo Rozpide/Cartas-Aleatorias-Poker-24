69ae2f46128217809805cd98edfd1d2e050620e1
Rozpi <100348581+Rozpide@users.noreply.github.com>
2024-12-07 19:23:40 +0000

    le añado comentarios al js

--- src/app.js
+++ src/app.js
@@ -3,10 +3,11 @@ import "bootstrap";
 import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
-
+// espera a que la pagina este cargada
 window.onload = function() {
   function generarCarta() {
-    let numeroAleatorioParaCarta = [
+    //defino las dos variables para las opciones de numero y palo
+    const numeroAleatorioParaCarta = [
       "A",
       "2",
       "3",
@@ -21,29 +22,29 @@ window.onload = function() {
       "Q",
       "K"
     ];
-    let numeroAleatorioParaPalo = [
+    const numeroAleatorioParaPalo = [
       { palo: "♠", clase: "spade" },
       { palo: "♣", clase: "club" },
       { palo: "♥", clase: "heart" },
       { palo: "♦", clase: "diamond" }
     ];
-
+    // funcion para generar un numero aleatori o dentro de un rango pendiente de asignar
     function randomCartas(min, max) {
       return Math.floor(Math.random() * (max - min) + min);
     }
-
-    let carta =
+    //variables que generan un numero y palo aleatorio
+    const carta =
       numeroAleatorioParaCarta[
         randomCartas(0, numeroAleatorioParaCarta.length)
       ];
-    let paloInfo =
+    const paloInfo =
       numeroAleatorioParaPalo[randomCartas(0, numeroAleatorioParaPalo.length)];
-
-    let numeroElemento = document.querySelector(".remplazar-numero");
-    let topPaloElemento = document.querySelector(".top-left");
-    let bottomPaloElemento = document.querySelector(".bottom-right");
-    let cardElemento = document.querySelector(".card");
-
+    // obtenemos las referencias a los elementos del Dom
+    const numeroElemento = document.querySelector(".remplazar-numero");
+    const topPaloElemento = document.querySelector(".top-left");
+    const bottomPaloElemento = document.querySelector(".bottom-right");
+    const cardElemento = document.querySelector(".card");
+    //verificamos que los elementos existen y los actualizamos
     if (
       numeroElemento &&
       topPaloElemento &&
@@ -54,16 +55,14 @@ window.onload = function() {
       topPaloElemento.innerHTML = paloInfo.palo;
       bottomPaloElemento.innerHTML = paloInfo.palo;
 
-      // Remover clases de palo anteriores
+      // ahora eliminamos clases de palo anteriores
       cardElemento.classList.remove("spade", "club", "heart", "diamond");
 
-      // Agregar la nueva clase de palo
+      // a continuacion añadimos la nueva clase de palo
       cardElemento.classList.add(paloInfo.clase);
-    } else {
-      console.error("Uno o más elementos no fueron encontrados en el DOM.");
     }
   }
-
+  // agrgo un evento listener al boton con unclick genera una carta
   document.querySelector("#boton").addEventListener("click", generarCarta);
 
   // Generar una carta al cargar la página
